0731e5bcbda279e69027e708ce66c24e53666d1a
DariaGiotina <giotinda.daria.b9k@student.ucv.ro>
2024-11-26 16:09:59 +0200

    week2

--- main.cpp
+++ main.cpp
@@ -1,33 +1,81 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct Probleme {
+    string nume_problema;
+    string specialitate_problema;
+    int ore_problema;
+};
+struct Doctor
+{
+    string name;
+    string specialty;
+    int ore = 8;
+};
 int main()
 {
-    ifstream inFile("input.txt");
+    ifstream inFile("input4_bonus.txt");
 
     int no_problems, no_doctors;
-    string name, speciality;
-    
+
     inFile >> no_problems;
+
+    vector<Probleme> probleme(no_problems);
 
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> probleme[i].nume_problema;
+        inFile >> probleme[i].specialitate_problema;
+        inFile >> probleme[i].ore_problema;
     }
 
     inFile >> no_doctors;
-
+    vector<Doctor> doctors(no_doctors);
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> doctors[i].name;
+        inFile >> doctors[i].specialty;
     }
+
+
+    for (int i = 0; i < no_problems; i++)
+    {
+        bool accepted = false;
+        for (int j = 0; j < no_doctors; j++)
+        {
+
+            find_if(doctors.begin(), doctors.end(), [&](Doctor& doctor) {
+                if (doctor.specialty == probleme[i].specialitate_problema && doctor.name != "taken" && doctor.ore >= probleme[i].ore_problema)
+                {
+                    cout << doctor.name << " " << probleme[i].nume_problema << endl;
+                    doctor.name = "taken";
+                    doctor.ore -= probleme[i].ore_problema;
+                    accepted = true;
+                    return true;
+                }
+                return false;
+                });
+            if (accepted)
+            {
+                break;
+            }
+        }
+
+    }
+
+    inFile.close();
 
     return 0;
 }
+
+
+//if (probleme[i].specialitate_problema == doctors[j].specialty && doctors[j].name != "taken")
+//{
+//
+//    cout << doctors[j].name << " " << probleme[i].nume_problema << endl;
+//    doctors[j].name = "taken";
+//}
