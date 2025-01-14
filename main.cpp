243a9baf1b6f982211645d2309d44701a3cf4ca1
DariaGiotina <giotina.daria.b9k@student.ucv.ro>
2025-01-14 13:13:40 +0200

    week 5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <queue>
+#include <map>
 
 using namespace std;
 
@@ -9,26 +11,34 @@ struct Probleme {
     string nume_problema;
     string specialitate_problema;
     int durata;
+    int prioritate;
+    int ora_sosire;
 };
 struct Doctor
 {
     string name;
-    string specialitate;
     int available_hours = 8; 
     vector<string> assigned_problems;
+	int nr_specializari;
+    int ora_actuala = 9;
 };
 int main()
 {
-    ifstream inFile("input4_bonus.txt");
+    ifstream inFile("input2.txt");
 
     int no_problems, no_doctors;
+    string nume_specializare;
 
     inFile >> no_problems;
 
     vector<Probleme> probleme(no_problems);
+    priority_queue<pair<int, int>> prioritate_queue;
+
+	std::map<std::string, vector<string>> specialitate_map;
 
     for (int i = 0; i < no_problems; i++) {
-        inFile >> probleme[i].nume_problema >> probleme[i].specialitate_problema >> probleme[i].durata;
+        inFile >> probleme[i].nume_problema >> probleme[i].specialitate_problema >> probleme[i].ora_sosire >> probleme[i].durata >> probleme[i].prioritate ;
+        prioritate_queue.push({ probleme[i].prioritate, i });
     }
 
     inFile >> no_doctors;
@@ -36,19 +46,42 @@ int main()
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> doctors[i].name;
-        inFile >> doctors[i].specialitate;
-    }
+        inFile >> doctors[i].nr_specializari;
+		for (int j = 0; j < doctors[i].nr_specializari; j++)
+		{
+			inFile >> nume_specializare;
+			specialitate_map[doctors[i].name].push_back(nume_specializare);
+		}
+
+	}
 
 
-    for (const auto& problem : probleme) {
+    while (!prioritate_queue.empty()) {
+        auto [prioritate, index] = prioritate_queue.top(); 
+        prioritate_queue.pop();
+
+        const string& specialitate = probleme[index].specialitate_problema;
+        bool problema_alocata = false;
+
         for (auto& doctor : doctors) {
-            if (doctor.specialitate == problem.specialitate_problema && doctor.available_hours >= problem.durata) {
-                doctor.assigned_problems.push_back(problem.nume_problema);
-                doctor.available_hours -= problem.durata;
-                break; 
+
+            if (find(specialitate_map[doctor.name].begin(), specialitate_map[doctor.name].end(), specialitate) != specialitate_map[doctor.name].end()) {
+
+                if (doctor.available_hours >= probleme[index].durata) {
+  
+                    doctor.assigned_problems.push_back(probleme[index].nume_problema);
+                    doctor.available_hours -= probleme[index].durata;
+                    problema_alocata = true;
+                    break;
+                }
             }
         }
+
+        if (!problema_alocata) {
+            cout << "Problema " << probleme[index].nume_problema << " nu a putut fi alocata.\n";
+        }
     }
+
 
     for (const auto& doctor : doctors) {
         cout << doctor.name << " " << doctor.assigned_problems.size();
@@ -65,9 +98,3 @@ int main()
 }
 
 
-//if (probleme[i].specialitate_problema == doctors[j].specialty && doctors[j].name != "taken")
-//{
-//
-//    cout << doctors[j].name << " " << probleme[i].nume_problema << endl;
-//    doctors[j].name = "taken";
-//}
